d4ba5e0e7809a774becd2536d0836ab2b16a4228
Nikita Kravtsov <deez999deez@gmail.com>
2026-06-01 20:11:52 +0300

    change hibernate cfg

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,22 +1,16 @@
 package mate.academy.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import java.util.List;
 
 @Entity
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @OneToMany(fetch = FetchType.EAGER)
     @JoinColumn(name = "shopping_cart_id")
     private List<Ticket> tickets;
     @OneToOne
